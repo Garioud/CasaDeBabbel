bbd904d42bb98dedee2f928c975e00bb10881a79
Vladimir Giraud <45130347+Dophix@users.noreply.github.com>
2020-06-06 20:22:15 +0200

    update verbe

--- Code/CasaDeBabbel/CasaDeBabbel/frmVerbe.Designer.cs
+++ Code/CasaDeBabbel/CasaDeBabbel/frmVerbe.Designer.cs
@@ -36,6 +36,15 @@
             this.btnHideWindow = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
             this.btnStart = new System.Windows.Forms.Button();
+            this.lblNomPersonne = new System.Windows.Forms.Label();
+            this.lblNumberExo = new System.Windows.Forms.Label();
+            this.lblExo = new System.Windows.Forms.Label();
+            this.lblDesc = new System.Windows.Forms.Label();
+            this.lblCours = new System.Windows.Forms.Label();
+            this.lblActLec = new System.Windows.Forms.Label();
+            this.lblActualCours = new System.Windows.Forms.Label();
+            this.lblAcLec = new System.Windows.Forms.Label();
+            this.pgB_Progres = new System.Windows.Forms.ProgressBar();
             this.panel4.SuspendLayout();
             this.panel3.SuspendLayout();
             this.SuspendLayout();
@@ -115,10 +124,120 @@
             this.btnStart.UseVisualStyleBackColor = true;
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
+            // lblNomPersonne
+            // 
+            this.lblNomPersonne.AutoSize = true;
+            this.lblNomPersonne.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNomPersonne.Location = new System.Drawing.Point(842, 38);
+            this.lblNomPersonne.Name = "lblNomPersonne";
+            this.lblNomPersonne.Size = new System.Drawing.Size(120, 23);
+            this.lblNomPersonne.TabIndex = 39;
+            this.lblNomPersonne.Text = "NomPersonne";
+            this.lblNomPersonne.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblNumberExo
+            // 
+            this.lblNumberExo.AutoSize = true;
+            this.lblNumberExo.BackColor = System.Drawing.Color.Transparent;
+            this.lblNumberExo.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumberExo.Location = new System.Drawing.Point(338, 107);
+            this.lblNumberExo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNumberExo.Name = "lblNumberExo";
+            this.lblNumberExo.Size = new System.Drawing.Size(61, 22);
+            this.lblNumberExo.TabIndex = 37;
+            this.lblNumberExo.Text = "label2";
+            // 
+            // lblExo
+            // 
+            this.lblExo.AutoSize = true;
+            this.lblExo.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblExo.Location = new System.Drawing.Point(241, 107);
+            this.lblExo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblExo.Name = "lblExo";
+            this.lblExo.Size = new System.Drawing.Size(89, 22);
+            this.lblExo.TabIndex = 36;
+            this.lblExo.Text = "Exercice:";
+            // 
+            // lblDesc
+            // 
+            this.lblDesc.AutoSize = true;
+            this.lblDesc.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDesc.Location = new System.Drawing.Point(403, 82);
+            this.lblDesc.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDesc.Name = "lblDesc";
+            this.lblDesc.Size = new System.Drawing.Size(50, 22);
+            this.lblDesc.TabIndex = 33;
+            this.lblDesc.Text = "label";
+            // 
+            // lblCours
+            // 
+            this.lblCours.AutoSize = true;
+            this.lblCours.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCours.Location = new System.Drawing.Point(227, 38);
+            this.lblCours.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCours.Name = "lblCours";
+            this.lblCours.Size = new System.Drawing.Size(172, 22);
+            this.lblCours.TabIndex = 31;
+            this.lblCours.Text = "Votre cours actuel :";
+            // 
+            // lblActLec
+            // 
+            this.lblActLec.AutoSize = true;
+            this.lblActLec.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblActLec.Location = new System.Drawing.Point(403, 60);
+            this.lblActLec.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblActLec.Name = "lblActLec";
+            this.lblActLec.Size = new System.Drawing.Size(61, 22);
+            this.lblActLec.TabIndex = 35;
+            this.lblActLec.Text = "label2";
+            // 
+            // lblActualCours
+            // 
+            this.lblActualCours.AutoSize = true;
+            this.lblActualCours.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblActualCours.Location = new System.Drawing.Point(403, 38);
+            this.lblActualCours.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblActualCours.Name = "lblActualCours";
+            this.lblActualCours.Size = new System.Drawing.Size(50, 22);
+            this.lblActualCours.TabIndex = 32;
+            this.lblActualCours.Text = "label";
+            // 
+            // lblAcLec
+            // 
+            this.lblAcLec.AutoSize = true;
+            this.lblAcLec.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAcLec.Location = new System.Drawing.Point(256, 60);
+            this.lblAcLec.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblAcLec.Name = "lblAcLec";
+            this.lblAcLec.Size = new System.Drawing.Size(143, 22);
+            this.lblAcLec.TabIndex = 34;
+            this.lblAcLec.Text = "Leçon actuelle :";
+            // 
+            // pgB_Progres
+            // 
+            this.pgB_Progres.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.pgB_Progres.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.pgB_Progres.Location = new System.Drawing.Point(407, 108);
+            this.pgB_Progres.Margin = new System.Windows.Forms.Padding(4);
+            this.pgB_Progres.Name = "pgB_Progres";
+            this.pgB_Progres.Size = new System.Drawing.Size(479, 21);
+            this.pgB_Progres.Step = 1;
+            this.pgB_Progres.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.pgB_Progres.TabIndex = 38;
+            // 
             // frmVerbe
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(1000, 600);
+            this.Controls.Add(this.lblNomPersonne);
+            this.Controls.Add(this.lblNumberExo);
+            this.Controls.Add(this.lblExo);
+            this.Controls.Add(this.lblDesc);
+            this.Controls.Add(this.lblCours);
+            this.Controls.Add(this.lblActLec);
+            this.Controls.Add(this.lblActualCours);
+            this.Controls.Add(this.lblAcLec);
+            this.Controls.Add(this.pgB_Progres);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel3);
@@ -131,6 +250,7 @@
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -143,5 +263,14 @@
         private System.Windows.Forms.Button btnHideWindow;
         private System.Windows.Forms.Button btnExit;
         private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblNomPersonne;
+        private System.Windows.Forms.Label lblNumberExo;
+        private System.Windows.Forms.Label lblExo;
+        private System.Windows.Forms.Label lblDesc;
+        private System.Windows.Forms.Label lblCours;
+        private System.Windows.Forms.Label lblActLec;
+        private System.Windows.Forms.Label lblActualCours;
+        private System.Windows.Forms.Label lblAcLec;
+        private System.Windows.Forms.ProgressBar pgB_Progres;
     }
 }
